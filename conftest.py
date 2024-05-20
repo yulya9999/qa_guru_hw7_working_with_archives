11994766ad121f81cbda4a370b11fb890379d93d
Yulia.Vdovina <Yulia.Vdovina@infotecs.ru>
2024-05-20 15:34:11 +0300

    Исправления по дз

--- conftest.py
+++ conftest.py
@@ -6,6 +6,7 @@ import pytest
 CURRENT_FILE = os.path.abspath(__file__)
 CURRENT_DIRECTORY = os.path.dirname(CURRENT_FILE)
 TMP_DIR = os.path.join(CURRENT_DIRECTORY, "tmp")
+RESOURCES_DIR = os.path.join(CURRENT_DIRECTORY, "resources")
 
 
 @pytest.fixture(scope="module", autouse=True)
@@ -13,14 +14,10 @@ def create_archive():
     if not os.path.exists("resources"):
         os.mkdir("resources")
 
-    RESOURCES_DIR = os.path.join(CURRENT_DIRECTORY, "resources")
-
     with zipfile.ZipFile(os.path.join(RESOURCES_DIR, "files.zip"), "w") as zf:
         for file in ["test.pdf", "test.xlsx", "test.csv"]:
             add_file = os.path.join(TMP_DIR, file)
             zf.write(add_file, os.path.basename(add_file))
 
     yield
-    # zf.close()
-    # os.remove(os.path.join(RESOURCES_DIR, "files.zip"))
     shutil.rmtree(RESOURCES_DIR)
--- test_archives.py
+++ test_archives.py
@@ -1,37 +1,35 @@
 import os
+import zipfile
 from zipfile import ZipFile
 from pypdf import PdfReader
 from openpyxl import load_workbook
 import csv
 from io import TextIOWrapper
-from conftest import CURRENT_DIRECTORY
-
-RESOURCES_DIR = os.path.join(CURRENT_DIRECTORY, "resources")
+from conftest import RESOURCES_DIR
 
 
-def test_archive_pdf():
-    with ZipFile(os.path.join(RESOURCES_DIR, "files.zip"), "r") as zip_file:
-        # print(zip_file.namelist())
-        reader = PdfReader(zip_file.open("test.pdf"))
-        text = reader.pages[1].extract_text()
-        assert "Работа подготовлена на кафедре прикладной математики" in text
+def test_read_file_pdf():
+    with (zipfile.ZipFile(os.path.join(RESOURCES_DIR, 'files.zip')) as zip_file):
+        with zip_file.open('test.pdf') as pdf_file:
+            reader = PdfReader(pdf_file)
+            text = reader.pages[1].extract_text()
+            assert "Работа подготовлена на кафедре прикладной математики" in text
 
 
 def test_archive_xlsx():
     with ZipFile(os.path.join(RESOURCES_DIR, "files.zip"), "r") as zip_file:
-        workbook = load_workbook(zip_file.open("test.xlsx"))
-        sheet = workbook.active
-        text = sheet.cell(row=15, column=3).value
-        assert "Значения искомых перемен." in text
+        with zip_file.open("test.xlsx") as xlsx_file:
+            workbook = load_workbook(xlsx_file)
+            sheet = workbook.active
+            text = sheet.cell(row=15, column=3).value
+            assert "Значения искомых перемен." in text
 
 
 def test_archive_csv():
     with ZipFile(os.path.join(RESOURCES_DIR, "files.zip"), "r") as zip_file:
         with zip_file.open("test.csv") as csv_file:
             csvreader = list(csv.reader(TextIOWrapper(csv_file, "Windows-1251"), delimiter=';'))
-            # print(list(csvreader))
             second_row = csvreader[1]
-            # print(second_row)
 
             assert "Произв. оборуд. в нормочасах" in second_row[0]
             assert second_row[3] == "1"
